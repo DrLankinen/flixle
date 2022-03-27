27f434de059fc95e4f2562e662d3e5f47a7131ad
Elias Lankinen <elias@lankinen.xyz>
2022-03-27 16:06:22 +0300

    Use correct Google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,9 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
